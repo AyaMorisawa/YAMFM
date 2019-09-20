655c7e6e38e8f91796ad9ed6bafad22be6622659
Aya Morisawa <AyaMorisawa4869@gmail.com>
2019-09-20 23:13:02 +0900

    Refactor

--- src/parse.ts
+++ src/parse.ts
@@ -6,72 +6,69 @@ export function parse(source: string): N.RootNode {
   return { type: 'root', children: parseInline(source) };
 }
 
-function fallback(fn: (() => boolean)[]): void {
-  fn.find(f => f());
-}
-
 function parseInline(source: string): N.MfmNode[] {
   const groupValueStack = new Stack<<R>(cont: <S, T>(t: { g: S.GroupT<S, T>, openingValue?: S }) => R) => R>();
   const resultStack = new Stack<Stack<N.MfmNode>>();
   resultStack.push(new Stack());
   let offset = 0;
-  while (offset < source.length) {
-    fallback([
-      () => {
-        if (!groupValueStack.empty()) {
-          const groupValue = groupValueStack.top();
-          const done = groupValue(({ g, openingValue }) => {
-            const res = g.closing({ text: source, offset });
-            if (res.status === 'succeed') {
-              groupValueStack.pop();
-              const children = resultStack.pop().toArray();
-              const node = g.gen({ type: g.type, children }, [openingValue, res.value]);
-              resultStack.top().push(node);
-              offset += res.length;
-              return true;
-            }
-          });
-          if (done) return true;
-        }
-      },
-      () => {
-        for (const group of S.groups) {
-          const done = group(g => {
-            const res = g.opening({ text: source, offset });
-            if (res.status === 'succeed') {
-              groupValueStack.push(cont => cont({ g, openingValue: res.value }));
-              resultStack.push(new Stack());
-              offset += res.length;
-              return true;
-            }
-          });
-          if (done) return true;
-        }
-      },
-      () => {
-        for (const primitive of S.primitives) {
-          const done = primitive(p => {
-            const res = p.parser({ text: source, offset });
-            if (res.status === 'succeed') {
-              const node = p.gen({ type: p.type }, res.value);
-              resultStack.top().push(node);
-              offset += res.length;
-              return true;
-            }
-          });
-          if (done) return true;
-        }
-      },
-      () => {
-        const siblings = resultStack.top();
-        if (siblings.empty() || siblings.top().type !== 'text') {
-          siblings.push(N.text(''));
-        }
-        (siblings.top() as N.TextNode).text += source[offset];
-        offset++;
-        return true;
-      },
-    ]);
-  }
+  while (offset < source.length) [tryGroupClosing, tryGroupOpenings, tryPrimitives, tryText].find(f => f());
   return resultStack.pop().toArray();
+
+  function tryGroupClosing() {
+    if (!groupValueStack.empty()) {
+      const groupValue = groupValueStack.top();
+      const done = groupValue(({ g, openingValue }) => {
+        const res = g.closing({ text: source, offset });
+        if (res.status === 'succeed') {
+          groupValueStack.pop();
+          const children = resultStack.pop().toArray();
+          const node = g.gen({ type: g.type, children }, [openingValue, res.value]);
+          resultStack.top().push(node);
+          offset += res.length;
+          return true;
+        }
+      });
+      if (done) return true;
+    }
+  }
+
+  function tryGroupOpenings() {
+    for (const group of S.groups) {
+      const done = group(g => {
+        const res = g.opening({ text: source, offset });
+        if (res.status === 'succeed') {
+          groupValueStack.push(cont => cont({ g, openingValue: res.value }));
+          resultStack.push(new Stack());
+          offset += res.length;
+          return true;
+        }
+      });
+      if (done) return true;
+    }
+  }
+
+  function tryPrimitives() {
+    for (const primitive of S.primitives) {
+      const done = primitive(p => {
+        const res = p.parser({ text: source, offset });
+        if (res.status === 'succeed') {
+          const node = p.gen({ type: p.type }, res.value);
+          resultStack.top().push(node);
+          offset += res.length;
+          return true;
+        }
+      });
+      if (done) return true;
+    }
+  }
+
+  function tryText() {
+    const siblings = resultStack.top();
+    if (siblings.empty() || siblings.top().type !== 'text') {
+      siblings.push(N.text(''));
+    }
+    (siblings.top() as N.TextNode).text += source[offset];
+    offset++;
+    return true;
+  }
 }
